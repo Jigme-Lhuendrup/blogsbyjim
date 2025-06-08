8cf82fe144ab4f85af8aeb66c93d58097317f9dd
Jigme-Lhuendrup <07230029.sherubtse@rub.edu.bt>
2025-06-08 15:46:45 +0600

    Add production dependencies

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const db = require('../config/database');
+const { db } = require('../config/database');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const path = require('path');
